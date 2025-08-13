2f134fa0a12dc30104b42afbe51da2170093696f
abulkalamazad1425 <bsse1425@iit.du.ac.bd>
2025-08-13 10:13:57 +0600

    commit source

---
+++ src/test/java/com/bsse_1401/pregnancy_risk_detector/PregnancyRiskDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsse_1401.pregnancy_risk_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PregnancyRiskDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
